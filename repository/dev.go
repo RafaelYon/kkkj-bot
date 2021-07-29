9fd5eecabe6cfaa50c9a8a4cb408165d22d88b74
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2021-07-28 21:29:11 -0300

    Added Dev repository

---
+++ repository/dev.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/RafaelYon/kkkj-bot/domain"
+)
+
+type DevSaver interface {
+	Save(dev *domain.Dev) error
+}
+
+type DevRetriver interface {
+	Get(name string) (*domain.Dev, error)
+}
+
+type DevSaveRetriver interface {
+	DevSaver
+	DevRetriver
+}
+
+type Dev struct {
+	devs map[string]*domain.Dev
+}
+
+func (d *Dev) Get(name string) (*domain.Dev, error) {
+	if dev, ok := d.devs[name]; !ok {
+		return nil, fmt.Errorf("DEV \"%s\" não encontrado", name)
+	} else {
+		return dev, nil
+	}
+}
+
+func (d *Dev) Save(dev *domain.Dev) error {
+	if d.devs == nil {
+		d.devs = make(map[string]*domain.Dev)
+	}
+
+	d.devs[dev.Name] = dev
+	return nil
+}
